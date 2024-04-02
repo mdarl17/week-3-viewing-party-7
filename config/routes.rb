c186d8cbaf11bd5336736e684b2a14dab7d550a4
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-01 23:01:51 -0600

    whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login_user'
   post '/users', to: 'users#create'
+
   get '/users/:id/movies', to: 'movies#index', as: 'movies'
   get '/users/:user_id/movies/:id', to: 'movies#show', as: 'movie'
 

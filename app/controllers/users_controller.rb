2b171b956c0c16c54345c9765636b17aee30f79e
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-01 18:07:00 -0600

    Update users#create with new fields for password and password confirmation.  Lot of conditional logic added to distinguish which error class caught the exception to provide users with more specific, helpful info; users are re_directed back to the new registration page (tried to have info still up so users only had to change invalid fields.  However render :new (which should be the same as register_path) was throwing errors in RSpec.  Error => expecting /registration, received /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,35 @@ class UsersController <ApplicationController
   end 
 
   def create 
-    user = User.create(user_params)
-    if user.save
-      redirect_to user_path(user)
-    else  
-      flash[:error] = user.errors.full_messages.to_sentence
+    user = User.new(user_params)
+    if user.save && user.authenticate(user_params[:password])
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to user_path(id: user.id)
+    elsif user.errors[:email].include?("has already been taken")
+      flash[:error] = "Email validation error: Email has already been taken"
       redirect_to register_path
-    end 
-  end 
+    elsif user.errors[:email].include?("can't be blank")
+      flash[:error] = "Email validation error: Email has already been taken"
+      redirect_to register_path
+    elsif user.errors[:name].any?
+      flash[:error] = "Name validation error: #{user.errors[:name].to_sentence}"
+      redirect_to register_path
+    elsif user.errors[:password].any?
+      flash[:error] = "password validation error: #{user.errors[:password].to_sentence}"
+      redirect_to register_path
+    else
+      flash[:error] = "Validation error occurred. Please check your input."
+      redirect_to register_path
+    end
+  end
+
+  def login
+
+  end
 
   private 
 
   def user_params 
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password)
   end 
 end 

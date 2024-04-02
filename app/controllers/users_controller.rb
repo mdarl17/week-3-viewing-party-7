639ee3566a198531fd35e53b3ef7a78a3b162d95
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-01 19:25:58 -0600

    Add error handling for: name, email, password, password_confirmation Can't be Blank; password and password_confirmation Don't Match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,31 +12,36 @@ class UsersController <ApplicationController
     if user.save && user.authenticate(user_params[:password])
       flash[:success] = "Welcome, #{user.name}!"
       redirect_to user_path(id: user.id)
+
     elsif user.errors[:email].include?("has already been taken")
       flash[:error] = "Email validation error: Email has already been taken"
       redirect_to register_path
+
     elsif user.errors[:email].include?("can't be blank")
-      flash[:error] = "Email validation error: Email has already been taken"
+      flash[:error] = "Email validation error: Email #{user.errors[:email].to_sentence}"
       redirect_to register_path
-    elsif user.errors[:name].any?
-      flash[:error] = "Name validation error: #{user.errors[:name].to_sentence}"
+
+    elsif user.errors[:name].include?("can't be blank")
+      flash[:error] = "Name validation error: Name #{user.errors[:name].to_sentence}"
       redirect_to register_path
-    elsif user.errors[:password].any?
-      flash[:error] = "password validation error: #{user.errors[:password].to_sentence}"
+
+    elsif user.errors[:password].include?("can't be blank")
+      flash[:error] = "Password validation error: Password #{user.errors[:password].to_sentence}"
       redirect_to register_path
+
+    elsif user.errors[:password_confirmation].include?("doesn't match Password")
+      flash[:error] = "Password validation error: Password Confirmation #{user.errors[:password_confirmation].to_sentence}"
+      redirect_to register_path
+
     else
       flash[:error] = "Validation error occurred. Please check your input."
       redirect_to register_path
     end
   end
-
-  def login
-
-  end
-
+  
   private 
 
   def user_params 
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
 end 

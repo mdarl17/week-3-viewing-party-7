0d89a18e88d2880e05350af8182ede187cf1e676
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-01 18:13:01 -0600

    New feature tests for registration page after implementing user authentication

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,19 +6,27 @@ RSpec.describe "User Registration" do
 
     fill_in :user_name, with: 'User One'
     fill_in :user_email, with:'user1@example.com'
+    fill_in :user_password, with: 'pass123'
+    
     click_button 'Create New User'
 
-    expect(current_path).to eq(user_path(User.last.id))
+    user = User.last
+
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq("pass123")
+    expect(current_path).to eq(user_path(user.id))
     expect(page).to have_content("User One's Dashboard")
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create(name: 'User One', email: 'notunique@example.com')
+    user_1 = User.create!(name: 'User One', email: 'notunique@example.com', password: 'pass123')
 
     visit register_path
     
     fill_in :user_name, with: 'User Two'
     fill_in :user_email, with:'notunique@example.com'
+    fill_in :user_password, with: 'pass456'
+
     click_button 'Create New User'
 
     expect(current_path).to eq(register_path)

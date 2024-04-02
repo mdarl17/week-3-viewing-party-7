88dcfa666b7c155c9095bdf1a67182746111af14
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-01 23:43:19 -0600

    Add feature testing for sad path handling for both invalid password and invalid email user login

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -55,4 +55,50 @@ RSpec.describe 'Landing Page' do
 
     expect(current_path).to eq(user_path(User.last.id))
   end
+
+  it "will not give users access without a valid email" do 
+    user = User.create!(name: "Matt", email: "mdarl17@gmail.com", password: "pass123", password_confirmation: "pass123")
+
+    visit "/"
+
+    expect(page).to have_link("Log In")
+
+    click_link "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+
+    fill_in :email, with: "mdarls17@gmail.com"
+    fill_in :password, with: user.password
+
+    click_button "Log In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Invalid email or password")
+  end
+
+  it "will not give users access without a valid password" do 
+    user = User.create!(name: "Matt", email: "mdarl17@gmail.com", password: "pass123", password_confirmation: "pass123")
+
+    visit "/"
+
+    expect(page).to have_link("Log In")
+
+    click_link "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+
+    fill_in :email, with: "mdarl17@gmail.com"
+    fill_in :password, with: "pass12123"
+
+    click_button "Log In"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Invalid email or password")
+  end
 end

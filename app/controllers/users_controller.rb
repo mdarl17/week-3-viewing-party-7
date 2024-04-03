e9a5f94b988dc6863980a5796d70063056ed3cd9
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-02 21:40:52 -0600

    Prepend @ to users; @users needed by user dashboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,29 +8,29 @@ class UsersController <ApplicationController
   end 
 
   def create 
-    user = User.new(user_params)
+    @user = User.new(user_params)
     if user.save && user.authenticate(user_params[:password])
-      flash[:success] = "Welcome, #{user.name}!"
-      redirect_to user_path(id: user.id)
+      flash[:success] = "Welcome, #{@user.name}!"
+      redirect_to user_path(id: @user.id)
 
     elsif user.errors[:email].include?("has already been taken")
       flash[:error] = "Email validation error: Email has already been taken"
       redirect_to register_path
 
     elsif user.errors[:email].include?("can't be blank")
-      flash[:error] = "Email validation error: Email #{user.errors[:email].to_sentence}"
+      flash[:error] = "Email validation error: Email #{@user.errors[:email].to_sentence}"
       redirect_to register_path
 
     elsif user.errors[:name].include?("can't be blank")
-      flash[:error] = "Name validation error: Name #{user.errors[:name].to_sentence}"
+      flash[:error] = "Name validation error: Name #{@user.errors[:name].to_sentence}"
       redirect_to register_path
 
     elsif user.errors[:password].include?("can't be blank")
-      flash[:error] = "Password validation error: Password #{user.errors[:password].to_sentence}"
+      flash[:error] = "Password validation error: Password #{@user.errors[:password].to_sentence}"
       redirect_to register_path
 
     elsif user.errors[:password_confirmation].include?("doesn't match Password")
-      flash[:error] = "Password validation error: Password Confirmation #{user.errors[:password_confirmation].to_sentence}"
+      flash[:error] = "Password validation error: Password Confirmation #{@user.errors[:password_confirmation].to_sentence}"
       redirect_to register_path
 
     else
@@ -44,12 +44,14 @@ class UsersController <ApplicationController
   end
   
   def login_user
-    user = User.find_by(email: params[:email])
-    cookies.encrypted[:location] = params[:location]
-     
-    if user && user.authenticate(params[:password])
-      flash[:success] = "Welcome back, #{user.name}"
-      redirect_to user_path(user.id)
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      cookies.encrypted[:user_id] = @user.id
+      cookies.encrypted[:location] = params[:location]
+      cookies.encrypted[:expires_at] = 3.days.from_now
+      flash[:success] = "Welcome back, #{@user.name}"
+ 
+      redirect_to user_path(@user.id)
     else
       flash[:error] = "Invalid email or password"
       render :login_form

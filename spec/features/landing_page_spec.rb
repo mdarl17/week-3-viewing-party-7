b255d4a2d722e368da0c7810303a11a2af631220
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-03 12:34:39 -0600

    Housecleaning; delete test data that could pollute environment; log potential users out bc it is possible to start with users already logged in from prior tests

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -25,6 +25,8 @@ RSpec.describe 'Landing Page' do
   end 
 
   it 'if user is logged in, it displays existing users' do
+    visit logout_path
+
     user3 = User.create!(name: "User Three", email: "user3@test.com", password: "pass789", password_confirmation: "pass789")
     visit login_path 
 
@@ -44,11 +46,8 @@ RSpec.describe 'Landing Page' do
   end
 
   it "has a link to log in" do
-    User.delete_all
-    
     user = User.create!(name: "Matt", email: "mdarl17@gmail.com", password: "pass123", password_confirmation: "pass123")
-    
-    visit "/"
+    location = "Sloan's Lake, CO"
 
     expect(page).to have_link("Log In")
 
@@ -61,10 +60,11 @@ RSpec.describe 'Landing Page' do
 
     fill_in :email, with: user.email
     fill_in :password, with: user.password
+    fill_in :location, with: location
 
     click_button "Log In"
-
-    expect(current_path).to eq(user_path(User.last.id))
+    
+    expect(current_path).to eq(user_path(user.id))
   end
 
   it "if a user is logged in, the login/create new user links are disabled and logout link is enabled" do 
@@ -126,5 +126,21 @@ RSpec.describe 'Landing Page' do
       expect(page).to_not have_link(user.email)
       expect(page).to have_content(user.email)
     end
+  end
+
+  it "a visitor can not access a dashboard page, they must be logged in" do 
+    visit root_path
+
+    visit user_path(@user1.id)
+
+    expect(current_path).to eq(root_path)
+  end
+
+  it "if a visitor tries to access the dashboard, a message alerting them that they need to be logged in is displayed" do
+    visit logout_path
+    visit root_path
+    visit user_path(@user1.id)
+
+    expect(page).to have_content("You must be logged in to see the dashboard")
   end
 end

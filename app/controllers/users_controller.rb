f6fdaa204fd243ff5154f9a02fc118b1d2931137
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-02 22:01:31 -0600

    Prepend @ to user; @user needed by user dashboard view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,27 +9,27 @@ class UsersController <ApplicationController
 
   def create 
     @user = User.new(user_params)
-    if user.save && user.authenticate(user_params[:password])
+    if @user.save && @user.authenticate(user_params[:password])
       flash[:success] = "Welcome, #{@user.name}!"
       redirect_to user_path(id: @user.id)
 
-    elsif user.errors[:email].include?("has already been taken")
+    elsif @user.errors[:email].include?("has already been taken")
       flash[:error] = "Email validation error: Email has already been taken"
       redirect_to register_path
 
-    elsif user.errors[:email].include?("can't be blank")
+    elsif @user.errors[:email].include?("can't be blank")
       flash[:error] = "Email validation error: Email #{@user.errors[:email].to_sentence}"
       redirect_to register_path
 
-    elsif user.errors[:name].include?("can't be blank")
+    elsif @user.errors[:name].include?("can't be blank")
       flash[:error] = "Name validation error: Name #{@user.errors[:name].to_sentence}"
       redirect_to register_path
 
-    elsif user.errors[:password].include?("can't be blank")
+    elsif @user.errors[:password].include?("can't be blank")
       flash[:error] = "Password validation error: Password #{@user.errors[:password].to_sentence}"
       redirect_to register_path
 
-    elsif user.errors[:password_confirmation].include?("doesn't match Password")
+    elsif @user.errors[:password_confirmation].include?("doesn't match Password")
       flash[:error] = "Password validation error: Password Confirmation #{@user.errors[:password_confirmation].to_sentence}"
       redirect_to register_path
 

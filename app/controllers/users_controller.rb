adb6e61d9affc51f8c865e0769ee38fd1e0d8ce2
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-03 00:29:20 -0600

    Replace encrypted cookie with :user_id key to session with :user_id key

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController <ApplicationController
   def login_user
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      cookies.encrypted[:user_id] = @user.id
+      session[:user_id] = @user.id
       cookies.encrypted[:location] = params[:location]
       cookies.encrypted[:expires_at] = 3.days.from_now
       flash[:success] = "Welcome back, #{@user.name}"
@@ -56,6 +56,11 @@ class UsersController <ApplicationController
       flash[:error] = "Invalid email or password"
       render :login_form
     end
+  end
+
+  def logout
+    cookies.delete :location
+    reset_session
   end
   
   private 

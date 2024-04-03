47ae12b050ca5a7be35eca6526fbec6691afbdf8
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-03 12:39:59 -0600

    Add code to log user in so they can visit their dashboard (initial reqs did not have this permission restriction) instead of being re-directed to root_path and have tests fail; start w/ clean test data slate, delete movies and users from test database

--- spec/features/movie_index_spec.rb
+++ spec/features/movie_index_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies Index Page' do
+  User.delete_all
+  Movie.delete_all
+
   before do 
     @user1 = User.create(name: "User One", email: "user1@test.com", password: "pass123", password_confirmation: "pass123")
     i = 1
@@ -11,12 +14,19 @@ RSpec.describe 'Movies Index Page' do
   end 
 
   it 'shows all movies' do 
-    visit "users/#{@user1.id}"
+    location = "Sloan's Lake, CO"
+
+    visit login_path
+
+    fill_in :email, with: @user1.email
+    fill_in :password, with: @user1.password
+    fill_in :location, with: location
+
+    click_button 'Log In'
 
     click_button "Find Top Rated Movies"
 
     expect(current_path).to eq("/users/#{@user1.id}/movies")
-
     expect(page).to have_content("Top Rated Movies")
 
     Movie.all.each do |movie|
